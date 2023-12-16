46062fbcd50821a9b4fa247d7f6fe6284cc7c7c4
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-12-16 17:44:08 -0500

    update

--- myapp/components/Cards/UploadCard/UploadCard.js
+++ myapp/components/Cards/UploadCard/UploadCard.js
@@ -1,12 +1,124 @@
 'use client'
-import React from 'react'
+import React from 'react';
+import { FiLock, FiX } from 'react-icons/fi';
 
-const UploadCard = () => {
-  return (
-    <div>
-        UploadCard
-    </div>
-  )
+const UploadCard = React.memo(({ file, setFiles, index }) => {
+
+const validate = ({title, tags}) => {
+  const errors = {}
+  if(title.length > 100){
+    errors.title = 'title is too long'
+  }     else{
+    errors.title = ""
+  }
+
+
+  if(tags.length > 20){
+    errors.tags = 'tag is too long'
+  }else{
+    errors.tags = ""
+  }
+  return errors?.title || errors?.tags ? 'error' : 'success'
 }
 
-export default UploadCard
+
+  const handleChangeTitle = (e) => {
+    const {value} = e.target;
+    const newFile = {
+      ...file,
+      title: value,
+      status: validate({title: value, tags: file?.tags})
+    }
+    setFiles(files => files.map((item, i) => i === index ? newFile : item))
+  }
+
+  const handleInputTags = (e) => {
+    if(e.key === 'Enter' || e.key === ','){
+      let tag = e.target.value.trim().replace(/\s+/g, '').replace(',', '');
+
+      if(tag.length > 1 && !file?.tags?.includes(tag)){
+        const newFile = {
+          ...file,
+          tags: [...file.tags, tag],
+          status: validate({title: file.title, tags: [...file.tags, tag]})
+        }
+        setFiles(files => files.map((item, i) => i === index ? newFile : item))
+      }
+      e.target.value = '';
+    }
+  }
+
+  const handleRemoveTag = (tagToRemove) => {
+    const newTags = file.tags.filter((tag) => tag !== tagToRemove);
+    const newFile = {
+      ...file,
+      tags: newTags,
+      status: validate({ title: file.title, tags: newTags }),
+    };
+    setFiles((files) => files.map((item, i) => (i === index ? newFile : item)));
+  };
+
+  const handleChangePublic = () => {
+    setFiles(files => files.map((item, i) =>
+    i === index ? {...file, public: !file.public} : item
+    ))
+  }
+
+  const handleRemoveFile = () => {
+    setFiles(files => files.filter((_, i) => i !== index))
+  }
+  
+  return (
+    <div className={`flex flex-col ${file?.status}`}>
+         <div className="cursor-pointer text-red-500" onClick={handleRemoveFile}>
+          <FiX className="text-2xl mt-2 hover:text-red-700" />
+        </div>
+    <img src={file?.imgUrl} alt={file?.title || ''} width={200} height={280} title={file?.title} />
+
+    {file?.errMsg ? (
+      <div className='text-center'>
+        <h2 className='text-center'>Wrong File Type</h2>
+        <span>{file?.errMsg}</span>
+      </div>
+    ) : (
+      <div className=''>
+        <div className='flex flex-col' title={`${file?.title?.length} / 100`}>
+          <input
+            type='text'
+            autoComplete='off'
+            placeholder='Add A Title'
+            defaultValue={file?.title}
+            onChange={handleChangeTitle}
+            className='border border-gray-300 rounded p-2 mb-2 w-full focus:outline-none focus:border-primary transition-all duration-300'
+          />
+        </div>
+        <div className='flex flex-wrap mb-2' title={`${file?.tags?.length} / 5`}>
+          {file?.tags?.map((tag, tagIndex) => (
+            <div key={tagIndex} className='flex items-center mr-2 mb-2'>
+              <span className='p-2 rounded-xl bg-secondary text-white'>{tag}</span>
+              <span onClick={() => handleRemoveTag(tag)}>
+                <FiX className='ml-2 cursor-pointer text-gray-500 hover:text-red-500' />
+              </span>
+            </div>
+          ))}
+          <input
+            type='text'
+            autoComplete='off'
+            onKeyUp={handleInputTags}
+          />
+        </div>
+        <label className='flex items-center cursor-pointer mb-2'>
+          <input
+            onChange={handleChangePublic}
+            type='checkbox' checked={file?.public} className='mr-2' />
+          <span className='text-sm text-gray-800'>Make Public</span>
+          <FiLock className={`ml-2 ${file?.public ? 'text-green-500' : 'text-gray-500'}`} />
+        </label>
+     
+      </div>
+    )}
+  </div>
+  );
+});
+
+export default UploadCard;
--- myapp/components/Upload/Upload.js
+++ myapp/components/Upload/Upload.js
@@ -1,15 +1,34 @@
 'use client'
-import React, {useState} from 'react'
+import React, {useMemo, useState} from 'react'
 import UploadForm from '../Forms/UploadForm/UploadForm'
 import UploadCard from '../Cards/UploadCard/UploadCard'
 
 const Upload = () => {
   const [files, setFiles] = useState([])
+  const [loading, setLoading] = useState(false);
+  const count = useMemo(() => {
+    return files.filter(file => file?.status === 'success').length
+  }, [files])
+
+  const handleUpload = async () => {
+    const filesUpload = files.filter(file => file?.status === 'succss')
+    const formData = new FormData()
+    filesUpload.forEach(file => {
+      formData.append('files', file.filesUpload)
+
+      if(!file.title){
+        file.title = file.name.replace(/.(jpeg||jpg||png)$/gi,'')
+      }
+    })
+
+    const newFiles = filesUpload.map(file => ({...file, filesUpload: '', imgUrl: ''}))
+    console.log(newFiles)
+  }
   return (
     <section className='px-4 py-12'>
       <div className='flex flex-col max-w-screen-xl  mx-auto items-center'>
       <UploadForm setFiles={setFiles} />
-      <div className='masonry'>
+      <div className='masonry flex mt-8'>
       {
         files.map((file, index) => {
           return (
@@ -23,6 +42,19 @@ const Upload = () => {
         })
         }
       </div>
+      <button
+      style={{margin: '20px 0'}}
+      disabled={count <= 0 || count > 5 || loading}
+      onClick={handleUpload}
+      className='bg-grey px-2 py-4 hover:bg-gray-600'>
+        {
+          loading
+          ? "Loading..."
+          : count
+            ? `Submit ${count} photos`
+            :   'Submit to thejayadad'
+        }
+      </button>
       </div>
     </section>
   )
